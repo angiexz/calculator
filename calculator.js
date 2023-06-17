61d6e412d6a4a23f17320dc7dd9b865871c6e1b8
angiexz <97621483+angiexz@users.noreply.github.com>
2023-06-17 15:16:44 -0400

    set up of css and html

---
+++ calculator.js
@@ -0,0 +1,27 @@
+let num1 = null; num2 = null; op = null;
+
+function add(n1,n2){
+    return n1+n2;
+}
+
+function sub(n1,n2){
+    return n1/n2;
+}
+
+function mult(n1,n2){
+    return n1*n2;
+}
+
+function div(n1,n2){
+    return n1/n2;
+}
+
+function operate(){
+
+}
+
+//Add event listeners to all the buttons
+//need to create this class first
+document.querySelectorAll('.numbers');
+s.addEventListener('mouseover',()=>{
+})
