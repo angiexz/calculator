5e1aac5428aeabd5a10a0057cfd62cdfb6f808b4
angiexz <97621483+angiexz@users.noreply.github.com>
2023-06-17 16:45:26 -0400

    numbers now displayed on screen

--- calculator.js
+++ calculator.js
@@ -16,12 +16,25 @@ function div(n1,n2){
     return n1/n2;
 }
 
-function operate(){
+function operate(n1,n2,op){
+    if(op == '+'){
+        n1 = sol(n1,n2);
+    }
 
+    //etc etc
 }
 
 //Add event listeners to all the buttons
-//need to create this class first
-document.querySelectorAll('.numbers');
-s.addEventListener('mouseover',()=>{
-})
+let small_button= document.querySelectorAll('.small_button');
+Array.from('small_button');
+
+let d = document.getElementById('disp');
+
+//Add an event listener to every button
+for(let j = 0; j<small_button.length; j++){
+    let s = small_button[j];
+    let message = s.value;
+    s.addEventListener('click',()=>{
+        d.innerHTML = d.innerHTML + message;
+    })
+}
