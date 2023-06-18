cf3bb2d7732403ecabd92263f8ffcc24d91d04a0
angiexz <97621483+angiexz@users.noreply.github.com>
2023-06-17 23:04:39 -0400

    Finished. Fixed all math operations & colour scheme which is ugly but eh

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ function add(n1,n2){
 }
 
 function sub(n1,n2){
-    return n1/n2;
+    return n1-n2;
 }
 
 function mult(n1,n2){
@@ -15,57 +15,70 @@ function mult(n1,n2){
 function div(n1,n2){
     return n1/n2;
 }
+function cl(display){
+    display.innerHTML = "";
+}
+
+function del(display){
+    let txt= display.innerText;
+    let arr = Array.from(txt);
+    let n = arr.slice(0,arr.length-1);
+    let n_string = n.join("");
+    display.innerHTML= n_string;
+}
 
 function operate(full_s){
     Array.from(full_s);
+    let copy  = full_s;
     let found = false;
-    let op_index = null;
+    let op_num = null;
     let sol = null;
-    console.log(full_s);
-    console.log(full_s.length)
     for(let l = 0 ; l<full_s.length;l++){
         let letter = full_s[l];
-        console.log(letter);
-        if(letter = '+'){
+        if(letter == '+'){
             num1 = Number(full_s.slice(0,l));
             num2 = Number(full_s.slice(l+1));
             sol = add(num1,num2);
             found = true;
             console.log('num1' + num1);
             console.log('num2' + num2);
-            break;
+            op_num = op_num+1;
 
-        }else if (letter = '-'){
+        }else if (letter == '-'){
+            let negative = copy[l-1]=='+' || copy[l-1]=='-' || copy[l-1]=='x'|| copy[l-1]=='/';
+            if(l>0 && !negative){
             num1 = Number(full_s.slice(0,l));
-            num2 = Number(full_s.slice(l+1,array.length+1));
+            num2 = Number(full_s.slice(l+1));
             sol =sub(num1,num2);
             found = true;
-            console.log("o");
+            op_num = op_num+1;
+            }
     
-        }else if (letter = 'x'){
+        }else if (letter == 'x'){
             num1 = Number(full_s.slice(0,l));
-            num2 = Number(full_s.slice(l+1,array.length+1));
+            num2 = Number(full_s.slice(l+1));
             sol =mult(num1,num2);
             found = true;
-            console.log("o");
+            op_num = op_num+1;
 
-        }else if (letter = '/'){
+        }else if (letter == '/'){
             num1 = Number(full_s.slice(0,l));
-            num2 = Number(full_s.slice(l+1,array.length+1));
-            sol=mult(num1,num2);
+            num2 = Number(full_s.slice(l+1));
+            sol=div(num1,num2).toFixed(4);
             found = true;
-            console.log("o");
+            op_num = op_num+1;
         }
-        
-        console.log("fuck");
-        
+    }
 
+    if (found == false){
+        alert("You need an operator!");
     }
-    if (found = false){
-        console.log("ohno");
+    if (op_num >1){
+        alert("You can only have one operator at a time!");
     }
+
     document.getElementById('disp').innerHTML=sol;
-    console.log(sol);
+    console.log('solution'+sol);
     found = false;
 }
 
@@ -75,7 +88,7 @@ Array.from('small_button');
 
 let d = document.getElementById('disp');
 
-//Add an event listener to every number 
+//Add an event listener to every number & operator
 for(let j = 0; j<small_button.length; j++){
     let s = small_button[j];
     let message = s.value;
@@ -84,7 +97,7 @@ for(let j = 0; j<small_button.length; j++){
     })
 }
 
-//add event listner to = 
+//add event listener to = and start calculation
 let equal = document.getElementById('equal');
 equal.addEventListener('click',()=>{
     let display_message = document.getElementById('disp').innerText;
@@ -92,3 +105,14 @@ equal.addEventListener('click',()=>{
 })
 
 //error message if there is already one operator and you try to add another one
+
+//event listener to clear
+let clearing = document.getElementById('clear');
+clearing.addEventListener('click',()=>{
+    cl(d);
+})
+
+let dele = document.getElementById('delete');
+dele.addEventListener('click',()=>{
+    del(d);
+})
